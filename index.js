28e7ab1318ab0b9fd3b93011455b30f4005375bc
Gaurav Yadav <gauravyatwork@gmail.com>
2024-04-10 18:07:08 +0530

    fixed dequeue edge case

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class DLL{
         this.size++;
     }
     
-    normallyGet(){
+    peek(){
         if(!this.head){
             return null;
         }
@@ -43,73 +43,48 @@ class DLL{
     dequeue(){
         if(!this.head){
             return null;
+        }else if(!this.head.next){
+            const temp=this.head.data;
+            this.head=null;
+            this.tail = null;
+            this.size--;
+            return temp;
+        }else{
+            const temp=this.head.data;
+            this.head=this.head.next;
+            this.head.prev=null;
+            this.size--;
+            return temp;
         }
-
-        const temp=this.head.data;
-        this.head=this.head.next;
-        this.head.prev=null;
-        this.size--;
-        return temp;
-    }
-    toArray() {
-        let current = this.head;
-        const result = [];
-        while (current) {
-            result.push(current.data);
-            current = current.next;
-        }
-        return result;
     }
 }
 
 
 const queue= new DLL();
 
-app.post('/add',function(req,res){
-    const user=req.body;
-    queue.enqueue(user);
-    res.status(200).json({
-        message:"we've got the user in message queue",
-        queue: queue.toArray()
-    })
+
+app.post('/push',function(req,res){
+    const data=req.body;
+    console.log(data);
+    queue.enqueue(data);
+    res.status(200).json({ message:"Data Queued Successfully"});
 })
 
-app.get('/fetch',function(req,res){
-    const user=queue.normallyGet();
-    if(!user){
-        res.status(404).json({
-            message:"couldn't find user in message queue",
-            queue: queue.toArray()
-        })
-    }
-    res.status(200).json({
-        message:"fetched user from message queue",
-        queue: queue.toArray()
-    })
-})
+app.get('/peek',function(req,res){
+    const data=queue.peek();
+    if(!data) res.status(200).json(null);
+    else res.status(200).json(data);
+});
 
-app.get('/removeInOrder',function(req,res){
-    const user=queue.dequeue();
-    if(!user){
-        res.status(404).json({
-            message:"no user in message queue",
-            queue: queue.toArray()
-        })
-    }
-    res.status(200).json({
-        user,
-        message:"User removed from message queue",
-        queue: queue.toArray()
-    })
-})
-
+app.get('/pop',function(req,res){
+    const data=queue.dequeue();
+    if(!data) res.status(200).json(null);
+    else res.status(200).json(data);
+});
 
 app.get('/size',function(req,res){
-    res.status(200).json({
-        message:"Size of message queue",
-        size: queue.size
-    })
-}) 
+    res.status(200).json({size: queue.size})
+}); 
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000")
